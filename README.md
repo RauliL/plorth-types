ac850acdf2805713e0cfa04b60e7e75f01315ceb
Rauli Laine <rauli.laine@iki.fi>
2018-01-11 18:07:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plorth TypeScript declarations [![npm][npm-image]][npm-url]
+
+[npm-image]: https://img.shields.io/npm/v/plorth-types.svg
+[npm-url]: https://npmjs.org/package/plorth-types
+
+Collection of TypeScript type declarations to be used in [Plorth] parser
+and interpreter implementations.
+
+[Plorth]: http://plorth.org
---
+++ index.ts
@@ -0,0 +1,92 @@
+/**
+ * Enumeration of different value types used in Plorth.
+ */
+export enum PlorthValueType {
+  ARRAY = "array",
+  BOOLEAN = "boolean",
+  ERROR = "error",
+  NULL = "null",
+  NUMBER = "number",
+  OBJECT = "object",
+  QUOTE = "quote",
+  STRING = "string",
+  SYMBOL = "symbol",
+  WORD = "word"
+}
+
+/**
+ * Enumeration of different error codes used in Plorth.
+ */
+export enum PlorthErrorCode {
+  /** Syntax error. */
+  SYNTAX = 1,
+  /** Reference error. */
+  REFERENCE = 2,
+  /** Type error. */
+  TYPE = 3,
+  /** Value error. */
+  VALUE = 4,
+  /** Range error. */
+  RANGE = 5,
+  /** Import error. */
+  IMPORT = 6,
+  /** Unknown error. */
+  UNKNOWN = 100
+}
+
+export interface PlorthValue {
+  type: PlorthValueType;
+}
+
+export interface PlorthError extends PlorthValue {
+  type: PlorthValueType.ERROR;
+  code: PlorthErrorCode;
+  message?: string;
+}
+
+export interface PlorthBoolean extends PlorthValue {
+  type: PlorthValueType.BOOLEAN;
+  value: boolean;
+}
+
+export interface PlorthNumber extends PlorthValue {
+  type: PlorthValueType.NUMBER;
+  value: number;
+}
+
+export interface PlorthString extends PlorthValue {
+  type: PlorthValueType.STRING;
+  value: string;
+}
+
+export interface PlorthArray extends PlorthValue {
+  type: PlorthValueType.ARRAY;
+  elements: Array<PlorthValue | null>;
+}
+
+export interface PlorthObject extends PlorthValue {
+  type: PlorthValueType.OBJECT;
+  properties: { [key: string]: PlorthValue | null };
+}
+
+export interface PlorthError extends PlorthValue {
+  type: PlorthValueType.ERROR;
+  code: PlorthErrorCode;
+  message?: string;
+}
+
+export interface PlorthSymbol extends PlorthValue {
+  type: PlorthValueType.SYMBOL;
+  id: string;
+}
+
+export interface PlorthQuote extends PlorthValue {
+  type: PlorthValueType.QUOTE;
+  values: Array<PlorthValue | null>;
+}
+
+export interface PlorthWord extends PlorthValue {
+  type: PlorthValueType.WORD;
+  symbol: PlorthSymbol;
+  quote: PlorthQuote;
+}
